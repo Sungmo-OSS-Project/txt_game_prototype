c8fe8d1ac1e5fd9546aac3396409e2333b72c9ac
Kei-410 <smkoo0410@gmail.com>
2023-04-27 23:02:53 +0900

    변수 추가

--- player_status.py
+++ player_status.py
@@ -1,2 +1,5 @@
 current_health = 20
-state = {} #각종 긍정적/부정적 상태가 상태명: 상태 등급 형태로 딕셔너리에 저장
+positive_states = {} #각종 긍정적 상태가 상태명: 상태 레벨 형태로 딕셔너리에 저장
+negative_states = {} #각종 긍정적 상태가 상태명: 상태 레벨 형태로 딕셔너리에 저장
+weight = 0
+items = [] 
