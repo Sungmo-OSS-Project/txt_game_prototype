4af1a15af043de5b0572791aa8d01ccafe3b9bb4
Halozhan <juwonhwang0411@gmail.com>
2023-04-28 12:38:36 +0900

    Add 프로그램 기본구조 작성

---
+++ main.py
@@ -0,0 +1,51 @@
+def initializeMaintainance():
+    """
+    기초 정비
+    """
+    print("여기는 기초 정비에 대해 작성해요!")
+
+def explore():
+    """
+    탐사 출발
+    """
+    print("여기는 탐사 출발에 대해 작성해요!")
+
+def event():
+    """
+    이벤트
+    """
+    print("여기는 이벤트에 대해 작성해요!")
+
+def comeback():
+    """
+    쉘터 복귀
+    """
+    print("여기는 쉘터 복귀에 대해 작성해요!")
+
+def maintainance():
+    """
+    정비
+    """
+    print("여기는 정비에 대해 작성해요!")
+
+def main():
+    """
+    프로그램 시작
+    """
+    print("프로그램 시작")
+    
+    initializeMaintainance()
+
+    explore()
+
+    event()
+
+    comeback()
+
+    maintainance()
+
+    print("프로그램 종료")
+    ## 프로그램 종료
+
+if __name__ == "__main__":
+    main()
