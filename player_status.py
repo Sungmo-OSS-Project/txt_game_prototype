23bebd87079688041cf5235f3bb06f6f960550af
Halozhan <juwonhwang0411@gmail.com>
2023-04-30 00:27:48 +0900

    Add Player class in Player.py

--- player_status.py
+++
@@ -1,6 +0,0 @@
-current_health = 20
-positive_states = {} #각종 긍정적 상태가 상태명: 상태 레벨 형태로 딕셔너리에 저장
-negative_states = {} #각종 긍정적 상태가 상태명: 상태 레벨 형태로 딕셔너리에 저장
-weight = 0
-weight_limit = 20 #최대 무게 제한
-items = [] 
