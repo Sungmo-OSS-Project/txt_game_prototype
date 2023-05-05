859dc0addf74607238b822253df274fdfe4953cc
Halozhan <juwonhwang0411@gmail.com>
2023-05-06 05:21:06 +0900

    Update Player class annotations, buffs' type to list

--- Player.py
+++ Player.py
@@ -1,9 +1,12 @@
 from Item import Item
+from Buff import Buff
+from decoName import decoName
+
 class Player:
-    def __init__(self):
+    def __init__(self: "Player") -> "Player":
         """Player 생성"""
 
-        self.health: int = 20
+        self.health: int = int(20)
         """체력\n
         기본(시작) 값은 20으로 고정.\n
         체력이 0이 되는 경우 플레이어는 사망하며, 게임 오버.\n
@@ -11,12 +14,12 @@ class Player:
         특정 이벤트를 통하여 체력에 증감이 있을 수 있음.
         """
 
-        self.health_max: int = 20
+        self.health_max: int = int(20)
         """최대 체력\n
         특정 이벤트를 통하여 최대 체력에 증감이 있을 수 있음."""
 
-        self.buffs: dict = dict()
-        """각종 긍정적/부정적 상태. ("상태 명": 레벨)의 형태\n
+        self.buffs: list[Buff] = list()
+        """각종 긍정적/부정적 상태\n
         긍정적 상태(+) :
         각종 이벤트의 선택지(분기)에서 특정 상태가 존재할 경우 선택할 수 있는 분기가 활성화 또는 추가됨.\n
         부정적 상태(-) :
@@ -29,25 +32,25 @@ class Player:
         하지만 아이템 중 일회성 아이템의 경우, 해당 아이템 사용 시 소멸한다.
         """
 
-        self.weight: int = 0
+        self.weight: int = int(0)
         """무게\n
         아이템마다 무게가 다르며, 플레이어가 한 번에 소지할 수 있는 아이템 수량에 제한을 두는 기능이다.
         """
 
-        self.weight_max: int = 20
+        self.weight_max: int = int(20)
         """최대 무게 제한\n
         플레이어는 시작 시 최대무게(weight_max)가 설정된다.
         특정 이벤트를 통하여 최대 무게를 늘릴 수 있다.
         """
 
 
-    def getHealth(self) -> int:
+    def getHealth(self: "Player") -> int:
         """
         체력 값을 반환
         """
         return self.health
 
-    def setHealth(self, health: int) -> int:
+    def setHealth(self: "Player", health: int) -> "Player":
         """
         체력 값을 설정
         """
@@ -67,10 +70,8 @@ class Player:
                 print("□", end="")
         print()
         return None
-    def printHealth(self):
-        print("체력 [{} / {}]".format(self.health, self.health_max))
         
-    def getIsDied(self) -> bool:
+    def getIsDied(self: "Player") -> bool:
         """죽었는가 반환"""
         if self.health <= 0: # 체력이 0보다 작거나 같아지면 True
             return True
@@ -78,85 +79,64 @@ class Player:
             return False
 
 
-    def getHealthMax(self) -> int:
-        """
-        최대 체력 값을 반환
-        """
+    def getHealthMax(self: "Player") -> int:
+        """최대 체력 값을 반환"""
         return self.health_max
 
-    def setHealthMax(self, max: int) -> int:
-        """
-        최대 체력 값을 설정
-        """
+    def setHealthMax(self: "Player", max: int) -> "Player":
+        """최대 체력 값을 설정"""
         self.health_max = max
-        return self.health_max
+        return self
 
 
-    def getBuffs(self) -> dict:
-        """
-        버프 상태를 ("상태 명": 레벨) 형태의 dict로 반환
-        """
+    def getBuffs(self: "Player") -> dict:
+        """버프 반환"""
         return self.buffs
 
-    def setBuffs(self, buffs: dict) -> dict:
-        """
-        버프 상태를 ("상태 명": 레벨) 형태의 dict로 설정
-        """
+    def setBuffs(self: "Player", buffs: dict) -> "Player":
+        """버프 설정"""
         self.buffs = buffs
-        return self.buffs
+        return self
 
-    
-    def getInventory(self) -> list[Item]:
-        """
-        인벤토리 값 반환
-        """
+
+    def getInventory(self: "Player") -> list[Item]:
+        """인벤토리 값 반환"""
         return self.inventory
 
-    def setInventory(self, inventory: list) -> list:
-        """
-        인벤토리 값 설정
-        """
+    def setInventory(self: "Player", inventory: list) -> "Player":
+        """인벤토리 값 설정"""
         self.inventory = inventory
-        return self.inventory
+        return self
 
-    def printInventory(self):
-        """
-        인벤토리 출력
-        """
-        print("인벤토리")
+    def printInventory(self: "Player") -> None:
+        """인벤토리 출력"""
+        decoName("인벤토리")
         for item in self.inventory:
             print(f"- {item.getName()}(무게:{item.getWeight()})")
+        return None
 
-    
-    def getWeight(self) -> int:
-        """
-        무게 값 반환
-        """
+
+    def getWeight(self: "Player") -> int:
+        """무게 값 반환"""
         return self.weight
 
-    def setWeight(self, weight: int) -> int:
-        """
-        무게 값 설정
-        """
+    def setWeight(self: "Player", weight: int) -> "Player":
+        """무게 값 설정"""
         if weight >= 0 and weight <= self.weight_max:
             self.weight = weight
-            return self.weight
+            return self
         else:
             return "Out of Range"
 
 
-    def getWeightMax(self) -> int:
-        """
-        최대 무게 값 반환
-        """
+    def getWeightMax(self: "Player") -> int:
+        """최대 무게 값 반환"""
         return self.weight_max
 
-    def setWeightMax(self, max) -> int:
-        """
-        최대 무게 값 설정
-        """
+    def setWeightMax(self: "Player", max: int) -> "Player":
+        """최대 무게 값 설정"""
         self.weight_max = max
-        return self.weight_max
+        return self
 
 
 if __name__ == "__main__":
