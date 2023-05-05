3222ddf8abd98f1654b09031ebfd15ea7510adaf
Halozhan <juwonhwang0411@gmail.com>
2023-05-05 19:06:14 +0900

    Added decoName to print the step name

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ heal = Buff(name="치유", level=1)
 poison = Buff(name="독", level=-1)
 
 
+def decoName(name: str):
+    """
+    함수의 시작을 꾸며주는 데코레이터
+    """
+    for _ in range(5): print("-", end="")
+    print(name, end="")
+    for _ in range(5): print("-", end="")
+    print()        
+
+
 def initializeMaintenance():
     """기초 정비\n
     쉘터 출발 이전에 아이템을 소지할 수 있다.
     """
-    for _ in range(5): print("-", end="")
-    print("기초 정비", end="")
-    for _ in range(5): print("-", end="")
-    print()
-    
-    
+    decoName("기초 정비")
+
     print("체력: " + str(player.getHealth()) + "/" + str(player.getHealthMax()))
     # 주석 처리.
     # player.setItems({"구급키트" : 2, "주사기" : 3, "손전등" : 1})
@@ -55,10 +61,7 @@ def explore():
     """탐사 출발\n
     쉘터에서 출발하며 각종 이벤트에 노출된다.
     """
-    for _ in range(5): print("-", end="")
-    print("탐사 출발", end="")
-    for _ in range(5): print("-", end="")
-    print()
+    decoName("탐사 출발")
 
     while(True):
         inputs = input("탐사 출발하시겠습니까? (Y/N) >>").upper()
@@ -73,9 +76,7 @@ def event():
     물자 획득 이벤트, 물자 손실 이벤트, 체력 증감 이벤트, 상태 추가 및 제거 이벤트, 
     물자 교환 이벤트, 전투 이벤트 등의 각종 이벤트가 발생한다.
     """
-    for _ in range(5): print("-", end="")
-    print("이벤트", end="")
-    for _ in range(5): print("-", end="")
+    decoName("이벤트")
     print()
 
     print("어떤 이벤트가 발생하여 무언가를 선택했습니다!") # EVENT
@@ -89,10 +90,7 @@ def comeback():
     """쉘터 복귀\n
     플레이어는 특정 시점마다 '쉘터 복귀' 선택지를 선택하여 쉘터로 복귀할 수 있다.
     """
-    for _ in range(5): print("-", end="")
-    print("쉘터 복귀", end="")
-    for _ in range(5): print("-", end="")
-    print()
+    decoName("쉘터 복귀")
 
     while(True):
         inputs = input("쉘터 복귀하시겠습니까? (Y/N) >>").upper()
@@ -110,10 +108,7 @@ def maintenance():
     체력이 최대치까지 회복되며, 자신이 소지한 아이템을 저장, 판매, 폐기할 수 있다.\n
     플레이어가 모든 정비를 마친 경우 다시 '탐사 출발'을 선택하여 2 ~ 5의 과정을 반복한다.
     """
-    for _ in range(5): print("-", end="")
-    print("정비", end="")
-    for _ in range(5): print("-", end="")
-    print()
+    decoName("정비")
 
     if player.getIsDied():
         print("당신은 쉘터에 복귀하지 못하고 사망하였습니다.")
