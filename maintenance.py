cbb6a85f1ac200f45a58b8c7e5bf3deded39f891
Kei-410 <smkoo0410@gmail.com>
2023-05-09 23:03:30 +0900

    Add 최대 체력 회복 to maintenance.py

--- maintenance.py
+++ maintenance.py
@@ -7,6 +7,7 @@ def maintenance(player: Player):
     플레이어가 모든 정비를 마친 경우 다시 '탐사 출발'을 선택하여 2 ~ 5의 과정을 반복한다.
     """
     print("정비")
-
-    player.printHealth()
-    player.printInventory()
+    player.setHealth(player.getHealthMax()) #최대체력까지 체력 회복
+    player.printHealth() #체력 표시
+    player.printInventory() #인벤토리 표시
+    #인벤토리 관리 추가 예정
