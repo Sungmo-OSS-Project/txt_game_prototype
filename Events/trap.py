b5f31f008c64a5edadff1599f6100c68582c4ea9
leeyoungwoo <dydwnsglfl@naver.com>
2023-05-16 22:56:37 +0900

    덫에 걸려 hp 감소

--- Events/trap.py
+++ Events/trap.py
@@ -13,6 +13,6 @@ class Fall_by_rock(Event):
         """self.player는 메시지를 전달할 객체가 있어야하니 반드시 파라미터로 받아야함."""
 
     def trigger(self) -> None:
-        print("당신은 길을 가다 다른 생존자가 둔 덫에 걸렸씁니다.\n체력이 2 감소합니다.")
+        print("당신은 길을 가다 다른 생존자가 설치한 덫에 걸렸습니다.\n체력이 2 감소합니다.")
         self.player.setHealth(self.player.getHealth() - 2)
         self.player.printHealth()
