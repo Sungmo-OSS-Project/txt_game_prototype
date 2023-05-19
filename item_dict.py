562494e0a6c125281c48fd1da8fe701818bb4790
leeyongwooo <130113148+leeyongwooo@users.noreply.github.com>
2023-05-19 11:48:22 +0900

    식량 세분화를 위한 삭제, 토마토 통조림 추가.
    식량 세분화를 위해 식량을 지우고 토마토 통조림을 넣었습니다. 다들 식량 세분화를 해주시면 감사하겠습니다.

--- item_dict.py
+++ item_dict.py
@@ -9,7 +9,7 @@ sample = Item(name = "아이템이름", weight = int())
 #변수명 
 knife = Item(name = "나이프", weight = 1)
 water_bottle = Item(name = "생수", weight = 2)
-food = Item(name = "식량", weight = 2)
 medikit = Item(name = "구급상자", weight = 4)
 crowbar = Item(name="쇠지렛대", weight=3)
 flashLight = Item(name="손전등", weight=2)
+tomato_can = Item(name="토마토 통조림", weight=2)
