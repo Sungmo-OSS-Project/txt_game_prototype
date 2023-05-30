50d2c73d45a6384b87419b8d163361e2870f625d
Halozhan <juwonhwang0411@gmail.com>
2023-05-30 10:44:14 +0900

    Item 클래스의 속성의 은폐성을 강화하였습니다.

--- Item.py
+++ Item.py
@@ -1,31 +1,32 @@
 class Item():
     """Item Class"""
-    def __init__(self, category: str = str(), name: str = str(), weight: int = int()) -> None:
+    def __init__(self, category: str = str(), name: str = str(),
+                 weight: int = int()) -> None:
         """Item 생성"""
 
-        self.category: str = category
+        self.__category: str = category
         """Item의 종류"""
 
-        self.name: str = name
+        self.__name: str = name
         """Item의 이름"""
 
-        self.weight: int = weight
+        self.__weight: int = weight
         """Item의 무게"""
 
     def getName(self) -> str:
         """Item의 이름을 반환"""
-        return self.name
+        return self.__name
 
     def setName(self, name: str) -> "Item":
         """Item의 이름을 설정"""
-        self.name = name
+        self.__name = name
         return self
 
     def getWeight(self) -> int:
         """Item의 무게를 반환"""
-        return self.weight
+        return self.__weight
 
     def setWeight(self, weight: int) -> "Item":
         """Item의 무게를 설정"""
-        self.weight = weight
+        self.__weight = weight
         return self
