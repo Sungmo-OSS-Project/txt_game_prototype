47d6edcddd9a043f1f9c8ebb4bd9cc058e1f4b14
murahide104 <128482232+murahide104@users.noreply.github.com>
2023-05-30 22:43:45 +0900

    main.py 일본어 코멘트 추가

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Player import Player, Death
 from Storage import Storage
 
-from initializeMaintenance import initializeMaintenance  # 기초 정비
-from explore import explore  # 탐사 출발
-from event import event  # 이벤트
-from comeback import comeback  # 쉘터 복귀
-from maintenance import maintenance  # 정비
+from initializeMaintenance import initializeMaintenance  # 기초 정비　/　基礎メンテナンス
+from explore import explore  # 탐사 출발　/　探索出発
+from event import event  # 이벤트　/　イベント
+from comeback import comeback  # 쉘터 복귀　/　シェルター帰還
+from maintenance import maintenance  # 정비　/　メンテナンス
 
 
 def main():
@@ -13,21 +13,21 @@ def main():
     print("프로그램 시작")
 
     player = Player()
-    days = int(0)  # 지난 날짜 수
+    days = int(0)  # 지난 날짜 수　/　経過した日数
     shelterInventory = Storage()
 
     try:
         initializeMaintenance(player)
         while (True):
             days += 1
-            if explore(days):  # 탐험하면
+            if explore(days):  # 탐험하면　/　探検したら
                 while (True):
                     for _ in range(3):
-                        event(player)  # 이벤트가 발생하고
+                        event(player)  # 이벤트가 발생하고　/　イベントが発生して
 
-                    if comeback(player):  # 쉘터에 복귀하면
+                    if comeback(player):  # 쉘터에 복귀하면　/　シェルターに帰還したら
                         break
-                maintenance(player, shelterInventory)  # 정비할 수 있음
+                maintenance(player, shelterInventory)  # 정비할 수 있음　/　メンテナンスが可能
     except Death as e:
         print(e)
     except Exception as e:
