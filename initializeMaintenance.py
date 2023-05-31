665442f54f95707fd0622fd709d8cdcd1db56ae8
Halozhan <juwonhwang0411@gmail.com>
2023-06-01 01:04:57 +0900

    Storage 클래스를 snake_case로 재구성했습니다.

--- initializeMaintenance.py
+++ initializeMaintenance.py
@@ -12,12 +12,12 @@ def initializeMaintenance(player: Player):
     player.printHealth()
 
     # 기본 아이템 제공
-    player.inventory.addItem(item_dict.knife)
-    player.inventory.addItem(item_dict.tomato_can)
-    player.inventory.addItem(item_dict.water_bottle)
-    player.inventory.addItem(item_dict.flashLight)
-    player.inventory.addItem(item_dict.medikit)
+    player.inventory.add_item(item_dict.knife)
+    player.inventory.add_item(item_dict.tomato_can)
+    player.inventory.add_item(item_dict.water_bottle)
+    player.inventory.add_item(item_dict.flashLight)
+    player.inventory.add_item(item_dict.medikit)
 
     # 인벤토리 목록 출력
-    player.inventory.printStorage()
+    player.inventory.print_storage()
     print("을 가지고 쉘터로 이동합니다.")
--- maintenance.py
+++ maintenance.py
@@ -2,7 +2,7 @@ from Player import Player
 from Storage import Storage
 
 
-def maintenance(player: Player, shelterInventory: Storage):
+def maintenance(player: Player, shelter_inventory: Storage):
     """정비\n
     체력이 최대치까지 회복되며, 자신이 소지한 아이템을 저장, 판매, 폐기할 수 있다.\n
     플레이어가 모든 정비를 마친 경우 다시 '탐사 출발'을 선택하여 2 ~ 5의 과정을 반복한다.
@@ -12,4 +12,4 @@ def maintenance(player: Player, shelterInventory: Storage):
     player.printHealth()  # 체력 표시
 
     # 플레이어 인벤토리와 쉘터 보관함 간 아이템 이동 기능
-    player.inventory.communicateWith(shelterInventory)
+    player.inventory.communicate_with(shelter_inventory)
